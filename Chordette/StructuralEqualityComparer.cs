ac8b7f37a432a5614b900ff4936f8c2f847dc6ee
hexafluoride <xafluoride@gmail.com>
2019-11-01 09:35:57 -0500

    Add StructuralEqualityComparer for Dictionary<byte[], V>s

---
+++ Chordette/StructuralEqualityComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Chordette
+{
+    [Serializable]
+    class StructuralEqualityComparer : IEqualityComparer, IEqualityComparer<object>
+    {
+        public new bool Equals(object x, object y)
+        {
+            var s = x as IStructuralEquatable;
+            return s == null ? object.Equals(x, y) : s.Equals(y, this);
+        }
+
+        public int GetHashCode(object obj)
+        {
+            var s = obj as IStructuralEquatable;
+            return s == null ? EqualityComparer<object>.Default.GetHashCode(obj) : s.GetHashCode(this);
+        }
+    }
+}
