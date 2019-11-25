eba4b8da2a5491c61fe6e18571ce3702f7508acc
hexafluoride <xafluoride@gmail.com>
2019-11-24 21:29:12 -0600

    Add utility method

--- Chordette/Extensions.cs
+++ Chordette/Extensions.cs
@@ -92,5 +92,17 @@ namespace Chordette
 
             return new_arr;
         }
+
+        public static IEnumerable<T> ShuffleIterator<T>(this IEnumerable<T> source, Random random)
+        {
+            var buffer = source.ToList();
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                int j = random.Next(i, buffer.Count);
+                yield return buffer[j];
+
+                buffer[j] = buffer[i];
+            }
+        }
     }
 }
